6c29a8d3b560a6e684b72c36d1b48d0f647ed903
Chris Aasted <caasted@gmail.com>
2017-01-26 15:57:29 -0800

    bug: Update number of epochs in bboxCovnet

--- digit_recognition/mycode/bboxCovnet.py
+++ digit_recognition/mycode/bboxCovnet.py
@@ -130,7 +130,7 @@ clf.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'categ
 
 clf.fit(train_dataset, [train_sequences, train_labels[:,:,0], train_labels[:,:,1], train_labels[:,:,2], 
                         train_labels[:,:,3], train_labels[:,:,4], train_bboxes], 
-                        batch_size=100, nb_epoch=1, validation_split=0.03, verbose=0)
+                        batch_size=100, nb_epoch=250, validation_split=0.03, verbose=0)
 
 clf.save('SVHN-BB-1.h5')
 
