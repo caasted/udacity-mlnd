f5a785bf3aa031856c367b6830e0658561115edc
Chris Aasted <caasted@gmail.com>
2017-01-26 20:56:57 -0800

    feat: Reduce dense layer and epochs for SVHN

--- digit_recognition/mycode/SVHNcovnet.py
+++ digit_recognition/mycode/SVHNcovnet.py
@@ -83,7 +83,7 @@ bn4 = BatchNormalization()(do4)
 
 fl = Flatten()(bn4)
 
-d1 = Dense(3096)(fl)
+d1 = Dense(1024)(fl)
 a9 = Activation('relu')(d1)
 do9 = Dropout(0.25)(a9)
 bn9 = BatchNormalization()(do9)
@@ -119,7 +119,7 @@ clf.compile(loss='categorical_crossentropy', optimizer='adadelta',
 
 clf.fit(train_dataset, [train_sequences, train_labels[:,:,0], train_labels[:,:,1], train_labels[:,:,2], 
                         train_labels[:,:,3], train_labels[:,:,4]], 
-                        batch_size=100, nb_epoch=250, validation_split=0.03, verbose=2)
+                        batch_size=100, nb_epoch=50, validation_split=0.03, verbose=2)
 
 clf.save('SVHN-1.h5')
 print "Training complete."
